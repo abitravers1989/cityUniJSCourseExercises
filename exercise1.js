99d60b80ddab51b5f85f5203f0f20d9eb9bfcf60
abitravers1989 <atravers4007@gmail.com>
2018-10-05 12:05:22 +0100

    exercise 1 done

--- exercise1.js
+++ exercise1.js
@@ -1,6 +1,6 @@
 
-const namesArray = ['John', 'Ruth', 'Peter'];
-const dateOfBirthArray = [23, 35, 40];
+const namesArray = ['John', 'Ruth', 'oldest', 'Peter'];
+const dateOfBirthArray = [23, 35, 65, 40];
 
 
 const calculateOldest = (names, dateOfBirth) => {
@@ -19,5 +19,26 @@ const calculateOldest = (names, dateOfBirth) => {
 
 const oldest = calculateOldest(namesArray, dateOfBirthArray);
 console.log(oldest);
+
+const calculateYoungest = (names, dateOfBirth) => {
+    // console.log(dateOfBirth[0])
+    let youngest = dateOfBirth[0];
+    let index;
+    for (let i = 0; i < dateOfBirth.length; i++) {
+        if (dateOfBirth[i] < youngest) {
+            youngest = dateOfBirth[i]
+            index = i;
+        }
+    }
+    const youngestName = names[index]
+    return youngestName;
+
+}
+
+const namesArray2 = ['John', 'Ruth', 'Petere', 'hanna', 'cats'];
+const dateOfBirthArray2 = [23, 35, 15, 40, 65];
+
+const youngest = calculateYoungest(namesArray2, dateOfBirthArray2);
+console.log(youngest);
 
 //could sort the array then get first and last element.
