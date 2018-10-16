b3f91c47c4fc8d371ba32e42190e972a84e677d4
abitravers1989 <atravers4007@gmail.com>
2018-10-16 16:54:03 +0100

    doesn't work without determinging the youngest age first

--- Session2/exercise2.js
+++ Session2/exercise2.js
@@ -38,11 +38,14 @@ let ruth = new Person('Ruth', 55)
 let peter = new Person('Peter', 55)
 
 
+let youngestPerson = new Person('youngestPerson', 21)
+
+
 // Step 4: 
 
 // Add those three objects to an array that you have to create 
 
-let newArray = [john, ruth, peter];
+let newArray = [john, ruth, peter, youngestPerson];
 //console.log(newArray)
 
 
@@ -53,30 +56,36 @@ let newArray = [john, ruth, peter];
 
 
 
-let youngest = newArray[0].age
+// let youngest = newArray[0].age
 
-const isSmallest = (person) => {
-    // console.log(person.age);
-    // console.log(youngest)
-    if (person.age <= youngest) {
-        youngest = person
-    }
-    return youngest
-    console.log(youngest);
-}
-let youngerObject = newArray.filter(isSmallest)
-console.log(youngerObject)
-
-
-// 
-
-
-// let youngerObject = newArray.filter(person => person.age <= youngest)
+// const isSmallest = (person) => {
+//     // console.log(person.age);
+//     // console.log(youngest)
+//     if (person.age <= youngest) {
+//         youngest = person
+//     }
+//     //return youngest
+//     console.log(youngest);
+// }
+// let youngerObject = newArray.filter(isSmallest)
 // console.log(youngerObject)
+
+
+// let youngest = newArray[0].age
+// for (i = 0; i < newArray.length; i++) {
+//     if (newArray[i].age < youngest) {
+//         youngest = newArray[i].age;
+//     }
+// }
+
+
+let youngest = newArray[0].age
+let youngerObject = newArray.filter(person => person.age <= youngest)
+console.log(youngerObject)
 
 
 // Step 6: 
 
 // print the values here
 
-console.log(`Youngest Object Name: ${youngerObject.name}, Age: ${youngerObject.age}`)
+//console.log(`Youngest Object Name: ${youngerObject.name}, Age: ${youngerObject.age}`)
