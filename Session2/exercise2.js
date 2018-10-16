161de9f848096a8ac665e156a398ce75646079e6
abitravers1989 <atravers4007@gmail.com>
2018-10-16 12:13:31 +0100

    First attempt at youngest

--- Session2/exercise2.js
+++ Session2/exercise2.js
@@ -6,7 +6,7 @@
 
 // Age, Name 
 
- 
+
 //https://codeburst.io/jsnoob-ways-to-create-an-object-including-es6-way-565a0fdaddb7
 
 // Step 1: 
@@ -23,37 +23,50 @@ function Person(name, age) {
 // First object is john 
 
 let john = new Person('John', 25)
- 
-console.log(john)
+
 
 // Step 2: 
 
 // Second object is Ruth 
 
- let ruth = new Person('Ruth', 55)
- 
+let ruth = new Person('Ruth', 55)
+
 // Step 3: 
 
 // Third object is Peter 
 
 let peter = new Person('Peter', 55)
- 
+
 
 // Step 4: 
 
 // Add those three objects to an array that you have to create 
 
 let newArray = [john, ruth, peter];
- console.log(newArray)
- 
+//console.log(newArray)
+
 
 // Step 5: 
 
 // Using the filter method get the younger and older object  
 
- 
- 
+
+
+
+let youngest = newArray[0].age
+for (i = 0; i < newArray.length; i++) {
+    if (newArray[i].age < youngest) {
+        youngest = newArray[i].age;
+    }
+}
+
+console.log(youngest);
+
+let youngerObject = newArray.filter(person => person.age <= youngest)
+console.log(youngerObject)
 
 // Step 6: 
 
 // print the values here
+
+console.log(`Youngest Object Name: ${youngerObject.name}, Age: ${youngerObject.age}`)
