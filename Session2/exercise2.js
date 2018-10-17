0bf59c7049f25d68b1ae74a58cae0fa346b0a4a7
abitravers1989 <atravers4007@gmail.com>
2018-10-17 16:37:45 +0100

    adding reducer to calculate minMax from reducer.js file

--- Session2/exercise2.js
+++ Session2/exercise2.js
@@ -15,35 +15,12 @@ let youngestPerson = new Person('youngestPerson', 21)
 
 let newArray = [john, ruth, peter, youngestPerson];
 
-
-//why does function scope not inheret from global scope es5
-
-const calculateYoungest = () => {
-    //USE REDUCE with conditional (ternary) operator
-    // let youngest = newArray[0].age
-    // //console.log(this.name)
-    // //this should be var as global scoped 
-    // for (i = 0; i < newArray.length; i++) {
-    //     return (newArray[i].age < youngest ? youngest = newArray[i].age : youngest)
-    // }
-    let youngest = newArray[0].age
-    const reducer = (accumulator, currentValue) => { if (currentValue.age <= youngest) { youngest = currentValue.age } }
-    newArray.reduce(reducer)
-    return youngest;
-}
-
-
-let youngestObject = newArray.filter(persons => persons.age <= calculateYoungest())
-
-//oldest
-let oldest = newArray[0].age
-for (i = 0; i < newArray.length; i++) {
-    if (newArray[i].age > oldest) {
-        oldest = newArray[i].age;
+//have to give accumulated a value each time.
+const result = newArray.reduce((acc, cv) => {
+    return {
+        min: acc.min === 0 || cv.age < acc.min.age ? cv : acc.min,
+        max: acc.max === 0 || cv.age > acc.max.age ? cv : acc.max
     }
-}
-let oldestObject = newArray.filter(persons => persons.age >= oldest)
+}, { min: 0, max: 0 })
 
-console.log(`Youngest Object Name: ${youngestObject[0].name}, Age: ${youngestObject[0].age}`)
-console.log(`Oldest Object Name: ${oldestObject[0].name}, Age: ${oldestObject[0].age}`)
-
+console.log(result);
