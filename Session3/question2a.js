ead8796ad4b2a287ef7db46ee900dee50abbb604
abitravers1989 <atravers4007@gmail.com>
2018-10-18 17:41:18 +0100

    scoping exercise

---
+++ Session3/question2a.js
@@ -0,0 +1,19 @@
+var fullname = "john";
+
+var obj = {
+    fullname: 'jane',
+    ss: console.log(fullname),
+    prop: {
+        fullname: "tom",
+        getName: function () {
+            return this.fullname;
+        }
+    }
+}
+
+var test = obj.prop.getName;
+
+console.log(obj.prop.ss)
+//this is undefined at the first console.log so there is no fullname. it is not picking up john
+console.log(obj.prop.getName())
+//console.log(test())
