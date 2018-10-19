50d21c9bed01798fbb830a8caf481c36a776174d
abitravers1989 <atravers4007@gmail.com>
2018-10-19 14:08:47 +0100

    new test variable returns correct value

--- Session3/question2b.js
+++ Session3/question2b.js
@@ -11,9 +11,14 @@ var obj = {
     }
 }
 
+//The test() function returns the value: Tom.
 var test = obj.prop.getName;
-
 var binded = test.bind(obj.prop)
 console.log(binded())
+//or 
+var test1a = obj.prop.getName.bind(obj.prop)
+console.log(test1a());
 
-//The test() function returns the value: Tom.
+//A new function is created as a copy of getName (call it test2) which will return the value: Jane.
+var test2 = obj.prop.getName.bind(obj)
+console.log(test2())
