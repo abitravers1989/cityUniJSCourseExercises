6c3c821fc38822a8ffbc5170e42562c1592c2ee5
abitravers1989 <atravers4007@gmail.com>
2018-10-19 17:22:22 +0100

    add all the methods requested

--- Session3/question3.js
+++ Session3/question3.js
@@ -7,9 +7,23 @@ var calc = {
     },
     subtract: (number) => {
         calc.total -= number;
+    },
+    increment: () => {
+        calc.total += 1;
+    },
+    decrement: () => {
+        calc.total -= 1;
+    },
+    getTotal: () => {
+        return calc.total;
     }
 }
 calc.add(4)
 console.log(calc.total)
 calc.subtract(66)
 console.log(calc.total)
+calc.increment()
+console.log(calc.total)
+calc.decrement()
+console.log(calc.total)
+console.log(calc.getTotal())
