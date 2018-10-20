2f7efb3d25e26af755e4b639b5247bbd26c4525e
abitravers1989 <atravers4007@gmail.com>
2018-10-20 11:15:52 +0100

    g

--- Session3/question3.js
+++ Session3/question3.js
@@ -3,27 +3,35 @@
 var calc = {
     total: 0,
     add: (number) => {
-        calc.total += number;
+        return calc.total += number;
     },
     subtract: (number) => {
-        calc.total -= number;
+        return calc.total -= number;
     },
     increment: () => {
-        calc.total += 1;
+        console.log(calc.total)
+        return calc.total += 1;
     },
     decrement: () => {
-        calc.total -= 1;
+        return calc.total -= 1;
     },
     getTotal: () => {
         return calc.total;
     }
 }
-calc.add(4)
-console.log(calc.total)
-calc.subtract(66)
-console.log(calc.total)
-calc.increment()
-console.log(calc.total)
-calc.decrement()
-console.log(calc.total)
-console.log(calc.getTotal())
+result1 = calc.increment().add(10)
+console.log(result1);
+
+// // Chain some methods
+// result1 = calc.increment().add(10).getTotal();
+// console.log(result1); // 11
+// // reset total
+// calc.total = 0;
+// // Chain some methods
+// result2 = calc.add(7).subtract(3).getTotal();
+// console.log(result2); // 4
+// // reset total
+// calc.total = 0;
+// // Chain some methods
+// result3 = calc.increment().decrement().getTotal();
+// console.log(result3);
