ef4ed54dfd15973d031de96ec76c78ab3f2cc4cb
abitravers1989 <atravers4007@gmail.com>
2018-10-22 16:31:39 +0100

    remove arrow functions so it works

--- Session3/question3.js
+++ Session3/question3.js
@@ -1,40 +1,37 @@
 var calc = {
     total: 0,
-    add: (number) => {
-        calc.total += number;
+    add: function (number) {
+        this.total += number;
+        return this;
     },
-    subtract: (number) => {
-        return calc.total -= number;
+    subtract: function (number) {
+        this.total -= number;
+        return this;
     },
-    increment: {
-        func: () => {
-            this.total = calc.total
-            this.total += 1;
-        },
-        binded: this.func.bind(calc),
-        binded()
+    increment: function () {
+        this.total += 1;
+        return (this)
     },
-    decrement: () => {
-        return calc.total -= 1;
+    decrement: function () {
+        this.total -= 1;
+        return this;
     },
     getTotal: () => {
         return calc.total;
     }
 }
-result1 = calc.increment().add(10)
-console.log(result1);
-console.log(calc.total)
 
-// // Chain some methods
-// result1 = calc.increment().add(10).getTotal();
-// console.log(result1); // 11
-// // reset total
-// calc.total = 0;
-// // Chain some methods
-// result2 = calc.add(7).subtract(3).getTotal();
-// console.log(result2); // 4
-// // reset total
-// calc.total = 0;
-// // Chain some methods
-// result3 = calc.increment().decrement().getTotal();
-// console.log(result3);
+
+// Chain some methods
+result1 = calc.increment().add(10).getTotal();
+console.log(result1); // 11
+// reset total
+calc.total = 0;
+// Chain some methods
+result2 = calc.add(7).subtract(3).getTotal();
+console.log(result2); // 4
+// reset total
+calc.total = 0;
+// Chain some methods
+result3 = calc.increment().decrement().getTotal();
+console.log(result3);
